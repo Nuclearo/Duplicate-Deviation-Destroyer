a275b526a5bc3c11f97b134e177bb3a8f3bdb968
Nuclearo <Nuclearfury8@gmail.com>
2013-08-01 09:58:08 +0300

    Refactored code to use jQuery. Removed redundant functions. Removed clickOn(), replaced by jQuery's .click().

--- DevWatch_Duplicate_Destroyer.user.js
+++ DevWatch_Duplicate_Destroyer.user.js
@@ -4,7 +4,7 @@
 // @namespace   NuclearGenom
 // @description Removes duplicates from your dA inbox
 // @include     http://www.deviantart.com/messages/*
-// @version     0.91
+// @version     0.94
 // @lisence		GPL v2
 // @grant		none
 // ==/UserScript==
@@ -13,79 +13,86 @@
 Uses code from Timid Script - http://userscripts.org/users/TimidScript
 ****/
 
-var gmi = document.getElementById("gmi-ResourceStream");
+var gmi = $("#gmi-ResourceStream");
 var deviations = {};
-var boxNumber;
-var waitIntervalID;
+var deteled;
 var originURL;
-var buttonClass = "f1";
+var timeoutLength = 4000;
 
-function cleanInbox(button) {
+function cleanInbox() {
 	deviations = {};
-	buttonClass = "f1 active";
+	deviations.length = 0;
+	deteled=false;
+	$("#DuplicateDeleterButton").addClass("active").off("click");
 	originURL = document.URL;
 	cleanPage();
 }
 
+
 function cleanPage () {
 	try{
-		var boxes = gmi.getElementsByClassName("mcbox ch mcbox-thumb mcbox-thumb-deviation"); 
-		if (boxes[boxes.length-1].getElementsByClassName("tt-a")[0]&&boxes[boxes.length-1].getElementsByClassName("tt-a")[0].getAttribute){
-			clearInterval(waitIntervalID);
-			while (boxNumber < boxes.length){
-				var id = boxes[boxNumber].getElementsByClassName("tt-a")[0].getAttribute("collect_rid").match(/:(\d+)/)[1];
+		var boxes = gmi.find(".mcbox");	//get all the message boxes
+		deteled = false;
+		//the boxes will always be there, their content might be late, so we check first.
+		if (boxes.last().find(".mcb-title a").attr("href")){
+			boxes.find(".mcb-title a:not(.seen)").each(function(){
+				var id = $(this).attr("href").match(/\d+$/);
 				if (id in deviations){
-					clickOn(boxes[boxNumber].getElementsByClassName("mcx")[0]);
-					waitIntervalID = setInterval(cleanPage,500);
-					return;
+					console.log("duplicate: "+id);
+					$(this).offsetParent().find(".mcx").click();
+					deteled=true;
 				}
 				else{
+					$(this).addClass("seen");
 					deviations[id]=true;
-					boxNumber++;
+					deviations.length++;
 				}
+			});
+			if (deteled){ //If you deleted anything, continue on this page.
+				setTimeout(cleanPage,timeoutLength);
+				console.log("deleted something, reprocessing page from there. Unique devs:"+deviations.length)
 			}
-			if(getNextPage()){
-				boxNumber = 0;
-				waitIntervalID = setInterval(cleanPage,500);
+			else if(getNextPage()){ //If this page is done, get the nest page.
+				deteled=false;
+				setTimeout(cleanPage,timeoutLength);
+				console.log("got next page, continuing. Unique devs:"+deviations.length)
 			}
-			else{
+			else{ //If there is no next pageour work here is done.
+				console.log("done. Unique devs:"+deviations.length);
 				location.assign(originURL);
-				buttonClass = "f1";
+				$("#DuplicateDeleterButton").removeClass("active").on("click",cleanInbox);
 			}
 		}
+		else { //if the content wasn't loaded yet, wait a bit more.
+			setTimeout(cleanPage,timeoutLength);
+			console.log("Page not loaded yet. Unique devs:"+deviations.length);
+		}
 	}catch(error){
-		console.log("Type error caught, aborting.");
 		console.log(error);
-		clearInterval(waitIntervalID);
 	}
-}
-
-function getMessagePage (pageNum){
-	var messages = new Array();
-
 }
 
 function placeButton (){
 	if(document.URL.indexOf("www.deviantart.com/messages/#view=deviations")!==-1){
 		if (!document.getElementById("DuplicateDeleterButton")){
-			var mczone = document.getElementsByClassName("mczone-filter");
+			var mczone = $(".mczone-filter");
 			if(mczone.length===1){
-				var button = document.createElement("div");	//the delete button
-				button.id = "DuplicateDeleterButton";
-				button.setAttribute("class",buttonClass);
-				button.textContent = "Delete Duplicate Deviations";            
-				button.onclick = cleanInbox;
-				var spot = mczone[0].getElementsByTagName("tr")[0];
+				var button = $(document.createElement("div"));	//the delete button
+				button.attr("id","DuplicateDeleterButton");
+				button.addClass("f1");
+				button.text("Delete Duplicate Deviations");            
+				button.click(cleanInbox);
+				var spot = mczone.find("tr");
 				var parenTD = document.createElement("td"); //enclosing td for the button
 				parenTD.setAttribute("class","f");
-				parenTD.appendChild(button);
-				spot.insertBefore(parenTD, spot.lastChild.nextSibling);
+				$(parenTD).append(button);
+				spot.append(parenTD);
 				//create a separator
 				// var divl = document.createElement("div");
 				// divl.setAttribute("class","dvl");
 				// spot.insertBefore(divl,parenTD);
 			}
-			gmi = document.getElementById("gmi-ResourceStream");
+			gmi = $("#gmi-ResourceStream");
 		}
 	}
 	else if (document.getElementById("DuplicateDeleterButton")){
@@ -98,23 +105,18 @@ function placeButton (){
 
 function getNextPage()
 {
-	var next = document.getElementsByClassName("r page");
+	var next = $(".r.page");
 	//console.log(next);
 	if (next.length > 0)
 	{
-		next = next[0];
-		console.log(next.href);
-		clickOn(next);
+		next.click();
 		return true;
 	}
 	return false;
 }
 
-function clickOn (element){
-	var evt = document.createEvent("MouseEvents");
-	evt.initEvent("click", true, true);
-	element.dispatchEvent(evt);
-}
 
-window.onload = placeButton();
-document.addEventListener("DOMSubtreeModified", placeButton, true);
+$(placeButton);
+// document.addEventListener("DOMSubtreeModified", placeButton, true);
+obs = new window.MutationObserver(placeButton);
+obs.observe(document,{subtree:true, childList: true});
